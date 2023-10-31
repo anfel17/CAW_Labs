18eced645d4e3f3daa155ce0282a40cde69fa497
anfel17 <anfel.kasmi@univ-constantine2.dz>
2023-10-31 16:52:29 +0100

    adding new functionalities to the maze

--- CAW_Lab2.part2_Gr2_KASMI_BOUSNOBRA/maze.js
+++ CAW_Lab2.part2_Gr2_KASMI_BOUSNOBRA/maze.js
@@ -1,3 +1,30 @@
+// document.addEventListener("DOMContentLoaded",function(){
+//   var boundaries = document.querySelectorAll(".boundary");
+//   var maze = document.getElementById("maze");
+//   var end = document.getElementById("end");
+//   var start = document.getElementById("start");
+//   var GameOver = 0;
+//     maze.addEventListener("mouseover", function() {
+//     for (var i = 0; i < boundaries.length; i++) {
+//       boundaries[i].style.backgroundColor = "red";
+//       GameOver =1;
+//     }
+//     });
+//     if (GameOver === 0){
+//     end.addEventListener("mouseover", function() {
+//         alert("you win ")
+//          });
+//         }
+//         start.addEventListener("mousevor",function(){
+//             for (var i = 0; i < boundaries.length; i++) {
+//                 boundaries[i].style.backgroundColor = "white";
+//                 GameOver=0;
+//               }
+           
+//         }
+//         )
+    
+//   });
 //Exercice1
 /*document.addEventListener('DOMContentLoaded', function() {
     const boundaries = document.querySelectorAll('.boundary');
@@ -17,48 +44,48 @@
 
 //Exercice2
 
-/*document.addEventListener('DOMContentLoaded', function() {
-    const boundaries = document.querySelectorAll('.boundary');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const boundaries = document.querySelectorAll('.boundary');
 
-    boundaries.forEach(boundary => {
-        boundary.addEventListener('mouseover', function() {
-            boundaries.forEach(item => {
-                item.style.backgroundColor = 'red';
-            });
-        });
+//     boundaries.forEach(boundary => {
+//         boundary.addEventListener('mouseover', function() {
+//             boundaries.forEach(item => {
+//                 item.style.backgroundColor = 'red';
+//             });
+//         });
 
-        boundary.addEventListener('mouseout', function() {
-            boundaries.forEach(item => {
-                item.style.backgroundColor = '#eeeeee';
-            });
-        });
-    });
-});*/
+//         boundary.addEventListener('mouseout', function() {
+//             boundaries.forEach(item => {
+//                 item.style.backgroundColor = '#eeeeee';
+//             });
+//         });
+//     });
+// });
 
 //Exercice3
 
-/*document.addEventListener('DOMContentLoaded', function() {
-    const boundaries = document.querySelectorAll('.boundary');
-    const end = document.getElementById('end');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const boundaries = document.querySelectorAll('.boundary');
+//     const end = document.getElementById('end');
 
-    boundaries.forEach(boundary => {
-        boundary.addEventListener('mouseover', function() {
-            boundaries.forEach(item => {
-                item.style.backgroundColor = 'red';
-            });
-        });
+//     boundaries.forEach(boundary => {
+//         boundary.addEventListener('mouseover', function() {
+//             boundaries.forEach(item => {
+//                 item.style.backgroundColor = 'red';
+//             });
+//         });
 
-        boundary.addEventListener('mouseout', function() {
-            boundaries.forEach(item => {
-                item.style.backgroundColor = '#eeeeee';
-            });
-        });
-    });
+//         boundary.addEventListener('mouseout', function() {
+//             boundaries.forEach(item => {
+//                 item.style.backgroundColor = '#eeeeee';
+//             });
+//         });
+//     });
 
-    end.addEventListener('mouseover', function() {
-        alert('You win!');
-    });
-});*/
+//     end.addEventListener('mouseover', function() {
+//         alert('You win!');
+//     });
+// });
 
 //Exercice4
 
@@ -132,11 +159,20 @@
 //Exercice7
 document.addEventListener('DOMContentLoaded', function() {
     let gameStarted = false;
+    let wonCount = 0;
+    let lostCount = 0;
+    let bestTime = null;
+    let startTime = null;
+    let endTime = null;
     const boundaries = document.querySelectorAll('.boundary');
     const end = document.getElementById('end');
     const start = document.getElementById('start');
     const status = document.getElementById('status');
     const maze = document.getElementById('maze');
+    const winCountDisplay = document.getElementById('winCount');
+    const lostCountDisplay = document.getElementById('lostCount');
+    const timeDisplay = document.getElementById('timeDisplay');
+    const progressDisplay = document.getElementById('progressBar'); 
 
     boundaries.forEach(boundary => {
         boundary.addEventListener('mouseover', function() {
@@ -146,8 +182,10 @@ document.addEventListener('DOMContentLoaded', function() {
                 });
                 status.textContent = 'You lose!';
                 gameStarted = false;
+                lostCount++;
+                lostCountDisplay.textContent = lostCount;
             }
-        }); 
+        });
 
         boundary.addEventListener('mouseout', function() {
             boundaries.forEach(item => {
@@ -159,8 +197,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
     end.addEventListener('mouseover', function() {
         if (gameStarted) {
+            endTime = new Date();
+            const timeSpent = (endTime - startTime) / 1000;
+            if (bestTime === null || timeSpent < bestTime) {
+                bestTime = timeSpent;
+                timeDisplay.textContent = `Best Time: ${bestTime} seconds`;
+            }
             status.textContent = 'You win!';
             gameStarted = false;
+            wonCount++;
+            winCountDisplay.textContent = wonCount;
         }
     });
 
@@ -170,6 +216,7 @@ document.addEventListener('DOMContentLoaded', function() {
             boundary.style.backgroundColor = '#eeeeee';
         });
         status.textContent = 'Move your mouse over the "S" to begin.';
+        startTime = new Date();
     });
 
     maze.addEventListener('mouseleave', function() {
@@ -179,7 +226,21 @@ document.addEventListener('DOMContentLoaded', function() {
             });
             status.textContent = 'You lose!';
             gameStarted = false;
+            lostCount++;
+            lostCountDisplay.textContent = lostCount;
+        }
+    });
+
+    maze.addEventListener('mousemove', function(e) {
+        if (gameStarted) {
+            const mazeRect = maze.getBoundingClientRect();
+            const mouseX = e.clientX;
+            const percentage = ((mouseX - mazeRect.left) / mazeRect.width) * 110;
+            progressDisplay.style.width = percentage + '%'; 
         }
     });
 });
+
+
+
 
